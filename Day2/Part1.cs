06e05f84f803bac76a1b7c1c45492eb635acbab7
Robert <44657150+robert-leroy@users.noreply.github.com>
2018-12-08 23:06:29 -0500

    Add files via upload

---
+++ Day2/Part1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Advent_Day_2
+{
+    class Part1
+    {
+        public static int nCurrentLetter;
+
+        static void Main(string[] args)
+        {
+
+            int twos = 0;
+            int threes = 0;
+
+            List<char> ListUsedLetters = new List<char>();
+
+            string[] lines = File.ReadAllLines(@"C:\Advent\Day2.txt");
+
+            foreach (string boxID in lines)
+            {
+                int[] counts = new int[boxID.Length];
+                for (int x = 0; x < boxID.Length; x++)
+                {
+                    counts[x] = 1;
+                    nCurrentLetter = boxID[x];
+
+                    if (ListUsedLetters.Find(FindLetter) == 0)
+                    {
+                        ListUsedLetters.Add(boxID[x]);
+                        for (int y = x + 1; y < boxID.Length; y++)
+                        {
+                            if (boxID[x] == boxID[y])
+                                counts[x]++;
+                        }
+                    }
+                }
+
+                for (int z = 0; z < counts.Length; z++)
+                {
+                    if (counts[z] == 2)
+                    {
+                        twos++;
+                        break;
+                    }
+                }
+
+                for (int z = 0; z < counts.Length; z++)
+                {
+                    if (counts[z] == 3)
+                    {
+                        threes++;
+                        break;
+                    }
+                }
+
+                ListUsedLetters.Clear();
+            }
+
+            System.Console.WriteLine("CheckSum of " + twos.ToString() + " * " + threes.ToString() + " " + (twos * threes).ToString());
+            return;
+        }
+
+        private static bool FindLetter(char n)
+        {
+            return n == nCurrentLetter;
+        }
+    }
+}
---
+++ Day2/Part2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Advent_Day_2
+{
+    class Part2
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int diff = 0;
+
+            string[] boxes = File.ReadAllLines(@"C:\Advent\Day2.txt");
+
+            for (int x=0; x<boxes.Length - 1; x++)
+            {
+                string boxID1 = boxes[x];
+
+                for (int y = x + 1; y < boxes.Length; y++)
+                {
+
+                    string boxID2 = boxes[y];
+                    diff = 0;
+
+                    for (int z = 0; z < boxID1.Length && diff < 2; z++)
+                    {
+                        if (boxID1[z] != boxID2[z])
+                            diff++;
+                    }
+
+                    if (diff == 1)
+                    {
+                        System.Console.Write("Matching Box = ");
+                        for (int a=0; a<boxID1.Length; a++)
+                        {
+                            if (boxID1[a] == boxID2[a])
+                            {
+                                System.Console.Write(boxID1[a]);
+                            }
+                        }
+                        System.Console.WriteLine("");
+                        System.Console.WriteLine("Done");
+                    }
+                }
+            }
+
+            return;
+        }
+    }
+}
